23cca33c9ffe0db1e87bcf88acee242e967e162e
Justin Do <justin.do@hotmail.com>
2026-09-21 13:28:24 -0400

    Update equipe-ultime: cap-constrained lineup (104M), player photos, salaries, hide from sidebar

--- sidebar.js
+++ sidebar.js
@@ -21,7 +21,6 @@
         { href: '/mon-equipe/', icon: '👥', label: 'Mon équipe' },
       ]
     },
-    { href: '/equipe-ultime/', icon: '🏆', label: 'Équipe ultime' },
     { href: '/podcast/', icon: '🎙', label: 'Podcast' },
     { href: '/faq/',     icon: '❓', label: 'FAQ' },
   ];
